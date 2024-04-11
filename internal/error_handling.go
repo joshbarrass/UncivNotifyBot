542e047acbf8fd2f6ca75547b908ff3cb466115f
Joshua Barrass <josh.barrass.work@gmail.com>
2024-04-11 23:56:53 +0100

    Add functions for reporting errors and finding the logs later

---
+++ internal/error_handling.go
@@ -0,0 +1,31 @@
+package internal
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/google/uuid"
+	"github.com/joshbarrass/UncivNotifyBot/pkg/telegrambot"
+	"github.com/sirupsen/logrus"
+)
+
+// generate a unique error reference based on a UUID
+func generateErrorReference() string {
+	u := uuid.New()
+	raw := strings.ReplaceAll(u.String(), "-", "")
+	data, _ := hex.DecodeString(raw)
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+// sends a message to the user indicating that an error occurred and
+// providing a reference. A logger is then returned for further
+// logging with this reference.
+func reportError(bot *telegrambot.Bot, update tgbotapi.Update) *logrus.Entry {
+	ref := generateErrorReference()
+	msg := fmt.Sprintf(MSG_ERR_UNEXPECTED_FMT, ref)
+	bot.ReplyToMsg(update, msg)
+	return logrus.WithField("reference", ref)
+}
